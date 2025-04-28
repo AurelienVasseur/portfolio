7f6bb2079da116934b266c6b902c7e5b90fcd2df
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2025-04-28 20:01:02 +0200

    Menu / Intro transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,49 @@ import AboutMe from "@/components/sections/about-me";
 import ContactSection from "@/components/sections/contact-section";
 import TestimonialsSection from "@/components/sections/testimonials-section";
 import IntroductionSection from "@/components/sections/introduction-section";
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <div className="relative flex min-h-screen flex-col">
       {/* <SiteHeader /> */}
       {/* <DotBackground /> */}
       <main className="flex-1" style={{ background: "oklch(0.145 0 0)" }}>
-        <div className="dark bg-background text-foreground flex justify-center items-center h-screen relative">
+        <div
+          className="dark bg-background text-foreground flex justify-center items-center h-screen relative"
+          onWheel={(e) => {
+            if (e.deltaY > 0) {
+              // Scrolling down
+              document.getElementById("menu")?.scrollIntoView({
+                behavior: "smooth",
+              });
+            }
+          }}
+        >
           <HeroSection />
         </div>
-        <div className="h-[120vh] relative z-10  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/10 rounded-t-4xl flex flex-col justify-center items-center">
+        <motion.div
+          className="h-[120vh] relative z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/10 rounded-t-4xl flex flex-col justify-center items-center"
+          onWheel={(e) => {
+            if (e.deltaY < 0) {
+              // Scrolling up
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            } else if (e.deltaY > 0) {
+              // Scrolling down
+              document.getElementById("content")?.scrollIntoView({
+                behavior: "smooth",
+              });
+            }
+          }}
+          onViewportEnter={() => {
+            document.getElementById("menu")?.scrollIntoView({
+              behavior: "smooth",
+            });
+          }}
+        >
           <div
             id="menu"
             className="py-[10vh] h-[100vh] w-full flex flex-col justify-center items-center"
@@ -27,8 +60,10 @@ export default function Home() {
               <IntroductionSection />
             </div>
           </div>
-        </div>
-        <div className="bg-background relative  z-20  shadow-2xl">
+        </motion.div>
+        <div id="content" className="bg-background relative  z-20  shadow-2xl">
+          {/* <div className="h-screen w-full bg-red-500 relative z-5">
+          </div> */}
           <div className=" flex flex-col px-8 pt-10 space-y-40 mx-auto max-w-5xl">
             <SiteHeader />
 
