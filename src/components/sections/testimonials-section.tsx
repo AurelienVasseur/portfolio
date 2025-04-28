1a924df2e70b6ba8500d13a120ac496fb8af1597
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2025-04-28 16:07:47 +0200

    Testimonials section responsive

--- src/components/sections/testimonials-section.tsx
+++ src/components/sections/testimonials-section.tsx
@@ -7,12 +7,12 @@ import { caveat } from "@/app/fonts";
 export default function TestimonialsSection() {
   return (
     <section className="py-20 bg-transparent relative">
-      <div className="grid grid-cols-3 gap-2">
+      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4 xl:gap-2">
         <div className="container mx-auto px-4 col-span-1 ">
           <p
             className={cn(
               caveat.className,
-              "text-4xl transform -rotate-[15deg] -translate-x-1/3 whitespace-pre-line flex flex-col justify-center items-center text-center w-4/3 font-semibold"
+              "text-4xl transform xl:rotate-[-15deg] xl:-translate-x-1/3 whitespace-pre-line flex flex-col justify-center items-center text-center xl:w-4/3 font-semibold"
             )}
           >
             Still not convinced that I&apos;m the one who will save humanity
