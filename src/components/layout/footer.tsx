127945fbd9c8f58e9944c37d3d537192795f7190
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2025-05-02 18:35:46 +0200

    Footer translations added

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,13 @@
+import { useTranslations } from "next-intl";
+
 export function Footer() {
+  const t = useTranslations("HomePage");
+
   return (
     <footer className="px-10 mt-16">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          If you love palm trees and llamas, we&apos;ll be friends
+          {t("footer.text")}
         </p>
       </div>
     </footer>
