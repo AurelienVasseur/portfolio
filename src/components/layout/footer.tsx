78ec6370f65150092cc0e9c16a001b89da40fc8c
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2025-05-04 11:57:06 +0200

    Footer improved

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,15 +1,19 @@
 import { useTranslations } from "next-intl";
+import packageJson from "../../../package.json";
 
 export function Footer() {
   const t = useTranslations("HomePage");
+  const commitSha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7);
 
   return (
-    <footer className="px-10 mt-16">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          {t("footer.text")}
+    <footer className="px-10 mt-16 mb-10 text-xs leading-loose text-muted-foreground">
+      <div className="container flex flex-col items-center justify-center gap-4 md:h-24">
+        <p className="text-center">{t("footer.text")}</p>
+        <p className="text-center">
+          {t("footer.version", { version: packageJson.version })}
+          {commitSha && ` - ${t("footer.commit", { commit: commitSha })}`}
         </p>
       </div>
     </footer>
-  )
-} 
+  );
+}
